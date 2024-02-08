91294336180a29c3479b16d3f3c9a7b6b080ec45
Helga Reesalu <helga.reesalu@gmail.com>
2024-02-08 10:07:04 +0100

    Add project files.

---
+++ Infrastructure/UserEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure;
+
+public class UserEntity
+{
+    [Key]
+    [Column(TypeName = "uniqueidentifier")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string UserName { get; set; } = null!;
+
+    [Required]
+    [Column(TypeName = "varchar(100)")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [Column(TypeName = "nvarchar(100)")]
+    public string Password { get; set; } = null!;
+
+    public virtual ICollection<TaskEntity> Tasks { get; set; } = new HashSet<TaskEntity>();
+}
